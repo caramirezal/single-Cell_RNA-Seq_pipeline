e7a41a13e40fb1fc086f19ddc02b430542b541af
caramirezal <cramireza@ciencias.unam.mx>
2018-10-31 23:01:04 -0600

    add indexing and sorting bam files

--- pipeline.bash
+++ pipeline.bash
@@ -5,8 +5,8 @@ export PATH=$PATH:inst/seqtk/
 export PATH=$PATH:inst/TrimGalore-0.4.5/
 export PATH=$PATH:inst/FastQC
 export PATH=$PATH:inst/bowtie2-2.3.4.3-linux-x86_64/
-#export PATH=$PATH:~/.local/bin/cutadapt
 export PATH=$PATH:inst/subread-1.6.3-Linux-x86_64/bin/
+##export PATH=$PATH:~/.local/bin/cutadapt
  
 
 
@@ -22,21 +22,27 @@ trim_galore --nextera --paired data/fastq/E*1_slide.fastq data/fastq/E*1_slide.f
 echo 'trim_galore finished'
 
 ## quality control 
-echo '... fastqc quality control visualization'
+echo '... fastqc quality control visualization ...'
 fastqc data/fastq_processed/*.fq -o reports/
 echo 'fastqc finished'
 
 ## alignment
-echo '... Performing alignment with bowtie2'
+echo '... Performing alignment with bowtie2 ...'
 bowtie2 -x data/bowtie2_index/Mus_musculus_GRCm38 --threads 3 --end-to-end --very-fast -1 data/fastq_processed/*1.fq -2 data/fastq_processed/*2.fq -S data/sam/alignment.sam
 echo 'bowtie2 aligment finished'
 
 ## sam to bam
-echo '... Converting sam file to bam'
+echo '... Converting sam file to bam ...'
 samtools view -Sb data/sam/alignment.sam > data/bam/alignment.bam
 echo 'bam covertion finishes'
 
+## bam indexing and sorting
+echo '... bam file indexing and sorting ...'
+samtools sort -o data/bam/sorted.bam data/bam/alignment.bam
+echo 'bam file indexing and sorting finished'
+
 ## quantification
-echo 'couting reads using featureCounts'
-featureCounts -Q 30 -p -a data/gtf_reference/Mus_musculus.GRCm38.94.gtf -o data/quant/rna_quant.tsv data/bam/alignment.bam
+echo 'couting reads using featureCounts ...'
+featureCounts -Q 30 -p -a data/gtf_reference/Mus_musculus.GRCm38.94.gtf -o data/quant/rna_quant.tsv data/bam/sorted.bam
 echo 'Feature count finished'
+
