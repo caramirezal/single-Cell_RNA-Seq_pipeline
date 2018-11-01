20e6949cc6b9790dafffa9efaf42c2d5584e1b4a
caramirezal <cramireza@ciencias.unam.mx>
2018-10-31 22:44:42 -0600

    pipeline working

--- pipeline.bash
+++ pipeline.bash
@@ -6,7 +6,7 @@ export PATH=$PATH:inst/TrimGalore-0.4.5/
 export PATH=$PATH:inst/FastQC
 export PATH=$PATH:inst/bowtie2-2.3.4.3-linux-x86_64/
 #export PATH=$PATH:~/.local/bin/cutadapt
-#export PATH=$PATH:/usr/local/bin/subread-1.6.3-Linux-x86_64/bin/
+export PATH=$PATH:inst/subread-1.6.3-Linux-x86_64/bin/
  
 
 
@@ -27,10 +27,16 @@ fastqc data/fastq_processed/*.fq -o reports/
 echo 'fastqc finished'
 
 ## alignment
+echo '... Performing alignment with bowtie2'
 bowtie2 -x data/bowtie2_index/Mus_musculus_GRCm38 --threads 3 --end-to-end --very-fast -1 data/fastq_processed/*1.fq -2 data/fastq_processed/*2.fq -S data/sam/alignment.sam
+echo 'bowtie2 aligment finished'
 
 ## sam to bam
-#samtools view -Sb data/sam/alignment.sam > data/bam/alignment.bam
+echo '... Converting sam file to bam'
+samtools view -Sb data/sam/alignment.sam > data/bam/alignment.bam
+echo 'bam covertion finishes'
 
 ## quantification
-#featureCounts -Q 30 -p -a data/gtf_reference/Mus_musculus.GRCm38.94.gtf -o data/rna_quant.tsv data/bam/alignment.bam
+echo 'couting reads using featureCounts'
+featureCounts -Q 30 -p -a data/gtf_reference/Mus_musculus.GRCm38.94.gtf -o data/quant/rna_quant.tsv data/bam/alignment.bam
+echo 'Feature count finished'
